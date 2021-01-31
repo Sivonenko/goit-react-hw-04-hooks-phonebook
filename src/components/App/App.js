80e79c23f03e9ce35a41e3e3aee76f18706a63c3
Yulia <sivonenkoyulia@gmail.com>
2021-01-31 23:07:36 +0200

    add

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ContactForm from '../ContactForm';
 import ContactList from '../ContactList';
@@ -9,6 +9,29 @@ import s from './App.module.css';
 export default function App() {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  useEffect(() => {
+    const userContacts = localStorage.getItem('userContacts');
+    const parsedContacts = JSON.parse(userContacts);
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const userContacts = JSON.parse(localStorage.getItem('userContacts'));
+  //   if (userContacts) {
+  //     setContacts(userContacts)
+  //   }
+  // });
+
+  useEffect(() => {
+    localStorage.setItem('userContacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleCheckUContact = name => {
     const isExistContact = !!contacts.find(contacts => contacts.name === name);
@@ -34,48 +57,6 @@ export default function App() {
     );
   };
 
-  // componentDidMount = () => {
-  //   const userContacts = JSON.parse(localStorage.getItem('userContacts'));
-  //   if (userContacts) this.setState({ contacts: userContacts });
-  // };
-
-  // componentDidUpdate = (prevProps, prevState) => {
-  //   const { contacts } = this.state;
-  //   if (contacts !== prevState.contacts) {
-  //     localStorage.setItem('userContacts', JSON.stringify(contacts));
-  //   }
-  // };
-
-  // handleAddContact = newContact =>
-  //   this.setState(({ contacts }) => ({
-  //     contacts: [...contacts, newContact],
-  //   }));
-
-  // handleCheckUContact = name => {
-  //   const { contacts } = this.state;
-  //   const isExistContact = !!contacts.find(contacts => contacts.name === name);
-
-  //   isExistContact && alert('Contact is already exist');
-  //   return !isExistContact;
-  // };
-
-  // handleRemoveContact = id =>
-  //   this.setState(({ contacts }) => ({
-  //     contacts: contacts.filter(contact => contact.id !== id),
-  //   }));
-
-  // handleFilterChange = filter => this.setState({ filter });
-
-  //  const getVisibleContacts = () => {
-  //     return contacts.filter(contact =>
-  //       contact.name.toLowerCase().includes(filter.toLowerCase()),
-  //     );
-  //   };
-
-  // render() {
-  //   // const { filter } = this.state;
-  //   const visibleContacts = this.getVisibleContacts();
-
   return (
     <div className={s.app__wrap}>
       <ContactForm
@@ -83,7 +64,7 @@ export default function App() {
         onCheckContact={handleCheckUContact}
       />
 
-      <Filter value={filter} onChange={handleFilterChange} />
+      <Filter filter={filter} onChange={handleFilterChange} />
       <ContactList
         contacts={getVisibleContacts()}
         onRemove={handleRemoveContact}
